0f292ab56272786321760f174cdbcc66ce4c60c6
Clay Freeman <git@clayfreeman.com>
2022-05-12 14:19:42 -0500

    remove stale rule from help text

--- src/Command/ValidateCommand.php
+++ src/Command/ValidateCommand.php
@@ -57,7 +57,6 @@ class ValidateCommand extends Command {
       '',
       'A suitable message submission agent must satisfy the following criteria:',
       '',
-      ' * The server must not allow authentication via plain-text connection.',
       ' * The server must support a modern TLS encryption protocol (TLSv1.2 or TLSv1.3).',
       ' * The server must use a valid certificate, verifiable using the Mozilla CA bundle.',
       ' * The server must support the SMTP AUTH extension.',
